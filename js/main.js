f09fb9c79984bfa1cd074a0b1bba3678da16dd76
Ilya Tokovchuk <i.tokovchuk@gmail.com>
2020-06-06 00:21:24 +0300

    eslint fix

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
-// Const
+'use strict';
 
-var TYPES = ["palace", "flat", "house", "bungalo"];
-var CHECKINS = ["12:00", "13:00", "14:00"];
-var CHECKOUTS = ["12:00", "13:00", "14:00"];
-var FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-var PHOTOS = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-   "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-   "http://o0.github.io/assets/images/tokyo/hotel3.jpg"]
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var CHECKINS = ['12:00', '13:00', '14:00'];
+var CHECKOUTS = ['12:00', '13:00', '14:00'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 var LOCATION_MIN_X = 100;
 var LOCATION_MAX_X = 1100;
 var LOCATION_MIN_Y = 130;
 var LOCATION_MAX_Y = 630;
 var ADS_NUMBER = 8;
-
-
 
 // случайное число в заданом диапазоне
 
@@ -55,11 +53,11 @@ var generateAds = function (number) {
     var locationY = getRandomInt(LOCATION_MIN_Y, LOCATION_MAX_Y);
     var ad = {
       author: {
-        avatar: "img/avatars/user0"+ i +".png",
+        avatar: 'img/avatars/user0' + i + '.png',
       },
       offer: {
-        title: "Название объекта",
-        address: locationX + ", " + locationY,
+        title: 'Название объекта',
+        address: locationX + ', ' + locationY,
         price: getRandomInt(1, 1000),
         type: getRandomElement(TYPES),
         rooms: getRandomInt(1, 4),
@@ -67,7 +65,7 @@ var generateAds = function (number) {
         checkin: getRandomElement(CHECKINS),
         checkout: getRandomElement(CHECKOUTS),
         features: shuffleArray(FEATURES).slice(0, getRandomInt(1, FEATURES.length)),
-        description: "Описание",
+        description: 'Описание',
         photos: shuffleArray(PHOTOS).slice(0, getRandomInt(1, PHOTOS.length)),
       },
       location: {
@@ -84,32 +82,35 @@ var ads = generateAds(ADS_NUMBER);
 
 // убираем  класс map--faded
 
-var switchingActiveMap = function() {
-  var map = document.querySelector(".map");
-  map.classList.remove("map--faded");
+var switchingActiveMap = function () {
+  var map = document.querySelector('.map');
+  map.classList.remove('map--faded');
 };
 
 switchingActiveMap();
 
 // Создаем пины к нашим объктам жилья
 
-    // Находим темплейт
-var pinTemplate = document.querySelector("#pin")
+// Находим темплейт
+
+var pinTemplate = document.querySelector('#pin')
   .content
-  .querySelector(".map__pin");
-    
-    //Делаем пин из темплейта и добавляем нужные свойства
+  .querySelector('.map__pin');
+
+// Делаем пин из темплейта и добавляем нужные свойства
+
 var renderPin = function (housing) {
   var pinElement = pinTemplate.cloneNode(true);
-  pinElement.style.left = housing.location.x - 25 + "px";
-  pinElement.style.top = housing.location.y - 35 + "px";
+  pinElement.style.left = housing.location.x - 25 + 'px';
+  pinElement.style.top = housing.location.y - 35 + 'px';
   pinElement.children[0].src = housing.author.avatar;
   pinElement.children[0].alt = housing.offer.title;
   return pinElement;
-}
+};
 
 // Добавляем пины на карту
-var mapPins = document.querySelector(".map__pins");
+
+var mapPins = document.querySelector('.map__pins');
 
 var fragment = document.createDocumentFragment();
 
