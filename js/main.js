d21443ebf4a9f671ce3be7223d1f1f0fb9a5b361
Ilya Tokovchuk <i.tokovchuk@gmail.com>
2020-06-07 23:09:10 +0300

    cardTemplate

--- js/main.js
+++ js/main.js
@@ -61,12 +61,12 @@ var generateAds = function (number) {
         address: locationX + ', ' + locationY,
         price: getRandomInt(1, 1000),
         type: getRandomElement(TYPES),
-        rooms: getRandomInt(1, 4),
-        guests: getRandomInt(1, 6),
+        rooms: getRandomInt(1, 20),
+        guests: getRandomInt(1, 50),
         checkin: getRandomElement(CHECKINS),
         checkout: getRandomElement(CHECKOUTS),
         features: shuffleArray(FEATURES).slice(0, getRandomInt(1, FEATURES.length)),
-        description: 'Описание',
+        description: 'Просто описание объекта: бла-бла-бла',
         photos: shuffleArray(PHOTOS).slice(0, getRandomInt(1, PHOTOS.length)),
       },
       location: {
@@ -116,3 +116,122 @@ var addPinsToMap = function (items) {
 var ads = generateAds(ADS_NUMBER);
 switchActiveMap();
 addPinsToMap(ads);
+
+// Создаем карточку объекта жилья
+
+var item = ads[0];
+var cardTemplate = document.querySelector('#card')
+  .content
+  .querySelector('.map__card');
+
+//
+var cardTitle = cardTemplate.querySelector('.popup__title');
+cardTitle.textContent = item.offer.title;
+
+//
+var cardAddress = cardTemplate.querySelector('.popup__text--address');
+cardAddress.textContent = item.offer.address;
+
+//
+var cardPrice = cardTemplate.querySelector('.popup__text--price');
+cardPrice.textContent = item.offer.price + '₽/ночь';
+
+//
+var cardType = cardTemplate.querySelector('.popup__type');
+if (item.offer.type === 'flat') {
+  cardType.textContent = 'Квартира'
+}
+if (item.offer.type === 'bungalo') {
+  cardType.textContent = 'Бунгало'
+}
+if (item.offer.type === 'house') {
+  cardType.textContent = 'Дом'
+}
+if (item.offer.type === 'palace') {
+  cardType.textContent = 'Дворец'
+};
+
+//
+var cardRoomsGuests = cardTemplate.querySelector('.popup__text--capacity');
+var rooms = item.offer.rooms;
+var guests = item.offer.guests;
+
+// функция для регулирования окончания в слове "комната"
+var roomsFlexNormalize = function (number) {
+  var forms = ["комнат", "комната", "комнаты"];
+  return flexNormalize(number, forms);
+};
+
+// функция для регулирования окончания в слове "гость"
+var guestsFlexNormalize = function (number) {
+  var forms = ["гостей", "гостя", "гостей"];
+  return flexNormalize(number, forms);
+};
+
+var flexNormalize = function (number, forms) {
+  number = Number(number);
+  if (number % 100 > 10 && number % 100 < 15) {
+    return forms[0];
+  }
+  var remainder = number % 10;
+  switch (true) {
+    case remainder === 0 || remainder > 4:
+      return forms[0];
+    case remainder === 1:
+      return forms[1];
+    default:
+      return forms[2];
+  }
+};
+
+var textRooms = roomsFlexNormalize (rooms);
+var textGuests = guestsFlexNormalize (guests);
+
+cardRoomsGuests.textContent = rooms + ' ' + textRooms + ' для ' + guests + ' ' + textGuests;
+
+//
+var cardTime = cardTemplate.querySelector('.popup__text--time');
+cardTime.textContent = 'Заезд после ' + item.offer.checkin + ', выезд до ' + item.offer.checkout;
+
+//
+var cardFeatures = cardTemplate.querySelector('.popup__features');
+for (var i = cardFeatures.children.length - 1; i >= 0; i--) {
+  var child = cardFeatures.children[i];
+  
+  child.parentElement.removeChild(child);
+}
+var featuresItems = item.offer.features;
+featuresItems.forEach (function (features) {
+cardFeatures.insertAdjacentHTML('afterbegin', '<li class="popup__feature popup__feature--' + features + '"></li>');
+});
+
+
+//
+var cardDescription = cardTemplate.querySelector('.popup__description');
+cardDescription.textContent = item.offer.description;
+
+//
+var cardAvatar = cardTemplate.querySelector('.popup__avatar');
+cardAvatar.src = item.author.avatar;
+
+//
+var cardPhotos = cardTemplate.querySelector('.popup__photos');
+for (var i = cardPhotos.children.length - 1; i >= 0; i--) {
+  var child = cardPhotos.children[i];
+  
+  child.parentElement.removeChild(child);
+}
+var photosItems = item.offer.photos;
+photosItems.forEach (function (photos) {
+cardPhotos.insertAdjacentHTML('afterbegin', '<img src="' + photos + '" class="popup__photo" width="45" height="40" alt="Фотография жилья">')
+});
+
+var map = document.querySelector('.map');
+var mapFilters = document.querySelector('.map__filters-container');
+map.insertBefore(cardTemplate, mapFilters);
+
+console.log(cardTemplate);
+
+
+
+
